cffe6a8f859d0c6fb33f4b6b5d5c2136ffc0d8ba
Carolina Coutinho <carolinafariacoutinho@gmail.com>
2022-06-02 14:55:40 -0300

    consulta.js

--- consulta.js
+++ consulta.js
@@ -27,33 +27,3 @@ async function main(){
     }
 }
 main().catch(console.error);
-
-/*console.log('Aluno: Carolina de Faria Coutinho');
-
-const {MongoClient} = require('mongodb');
-
-async function main(){
-
-    const uri = "mongodb+srv://Carolina:coutinho@cluster0.msaww.mongodb.net/ifsp?retryWrites=true&w=majority";
- 
-    const client = new MongoClient(uri);
- 
-    try {
-        await client.connect();
-        console.log('Servidor Conectado!')
-        const database = client.db("ifsp");
-
-        const contatos = database.collection("contatos");
-
-        const findResult = await contatos.find({}).toArray();
-        console.log('Foram encontrados os seguintes docs:');
-        console.log(findResult);
-
-
-    } catch (e) {
-        console.error(e);
-    } finally {
-        await client.close();
-    }
-}
-main().catch(console.error);*/
